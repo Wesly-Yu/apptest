253351a76c065097921653e3f2f2b52fd5fbfbc1
Wesly-Yu <1633235633@qq.com>
2020-08-16 11:52:28 +0800

    Initial commit

---
+++ src/main/java/base/baseSettings.java
@@ -0,0 +1,44 @@
+package base;
+
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class baseSettings {
+    private AndroidDriver driver;
+    @BeforeClass
+    public  void setUp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("platformVersion", "9.0");
+        desiredCapabilities.setCapability("deviceName", "p30");
+        desiredCapabilities.setCapability("appPackage", "com.xueqiu.android");
+        desiredCapabilities.setCapability("appActivity", ".common.MainActivity");
+        desiredCapabilities.setCapability("noReset", true);
+        desiredCapabilities.setCapability("newCommandTimeout", 6000);
+        desiredCapabilities.setCapability("unicodeKeyboard", true);
+        desiredCapabilities.setCapability("autoGrantPermissions",true);
+        desiredCapabilities.setCapability("automationName","UiAutomator2");
+        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+        driver = new AndroidDriver<>(remoteUrl,desiredCapabilities);
+    }
+    @Test
+    public void Login(){
+        driver.findElementByXPath("//android.widget.RelativeLayout[5]").click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElementByXPath("//*[contains(@text,'帐号密码登录')]").click();
+
+    }
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
---
+++ src/main/java/page/MainPage.java
@@ -0,0 +1,14 @@
+package page;
+
+import org.openqa.selenium.By;
+
+public class MainPage {
+    By profile = By.xpath("//android.widget.RelativeLayout[5]");
+    public static MainPage start(){
+        driver.start;
+        return new MainPage();
+    }
+    public  void profilepage(){
+
+    }
+}
